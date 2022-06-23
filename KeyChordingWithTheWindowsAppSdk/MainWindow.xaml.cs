a34ff52d29515ae357cfad33a46ece89c65bcd11
Ric DeHoff <ricd@eclectrics.com>
2022-06-23 08:57:35 -0700

    Add project files.

---
+++ KeyChordingWithTheWindowsAppSdk/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using Microsoft.UI.Xaml.Input;
+
+using Windows.System;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+
+namespace KeyChordingWithTheWindowsAppSdk
+{
+
+	public sealed partial class MainWindow // : Window
+	{
+		private readonly VirtualKey[] ChangeBackground = {VirtualKey.A, VirtualKey.S};
+
+		private readonly SolidColorBrush _redBrush = new(Colors.DarkRed);
+		private readonly SolidColorBrush _bludBrush = new(Colors.DarkBlue);
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OnRootGridKeyDown(object sender, KeyRoutedEventArgs e)
+		{
+			if (User32KeyboardHelper.AreKeysPressed(ChangeBackground, e.Key))
+			{
+				if (e.Key == VirtualKey.R)
+				{
+					_rootGrid.Background = _redBrush;
+				}
+				else if (e.Key == VirtualKey.B)
+				{
+					_rootGrid.Background = _bludBrush;
+				}
+			}
+
+			if (e.KeyStatus.IsMenuKeyDown)
+			{
+				_textBlock.Text = " Note: the Menu key is pressed.";
+			}
+		}
+
+		private void OnRootGridKeyUp(object sender, KeyRoutedEventArgs e)
+		{
+			_textBlock.Text = string.Empty;
+		}
+
+		private void OnRootGridPointerPressed(object sender, PointerRoutedEventArgs e)
+		{
+			var pressedKeys = User32KeyboardHelper.GetPressedKeys();
+
+			var builder = new StringBuilder();
+
+			builder.AppendLine("Pointer Pressed. The following keyboard keys were also pressed:");
+
+			foreach (var pressedKey in pressedKeys)
+			{
+				builder.AppendLine(pressedKey.ToString());
+			}
+
+			_textBlock.Text = builder.ToString();
+		}
+	}
+}
---
+++ KeyChordingWithTheWindowsAppSdk/User32KeyboardHelper.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using Windows.System;
+
+namespace KeyChordingWithTheWindowsAppSdk
+{
+	/// <summary>
+	/// Note, the Win32 nVirtKey values used in GetKeyState and the array indexing used in
+	/// GetKeyboardState in USER32.dll map exactly to the Windows.System.VirtualKey values.
+	/// </summary>
+	internal static class User32KeyboardHelper
+	{
+		#region Imports
+
+		/// <summary>
+		/// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getkeystate
+		/// </summary>
+		[DllImport("USER32.dll", SetLastError = true)]
+		private static extern short GetKeyState(int nVirtKey);
+
+		/// <summary>
+		/// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getkeyboardstate
+		/// </summary>
+		[DllImport("USER32.dll", SetLastError = true)]
+		private static extern bool GetKeyboardState(byte[] lpKeyState);
+
+		#endregion Imports
+
+		/// <summary>
+		/// If the high-order bit of the value returned by GetKeyState is 1,
+		/// the key is down; otherwise, it is up.
+		/// </summary>
+		public static bool IsKeyPressed(VirtualKey key) => GetKeyState((int)key) < 0;
+
+		/// <summary>
+		/// This might be useful if chording more than two keys.
+		/// </summary>
+		public static bool AreKeysPressed(VirtualKey[] keys)
+		{
+			var keyboardState = new byte[256];
+
+			// GetKeyboardState returns 'true' iff successful.
+			if (GetKeyboardState(keyboardState))
+			{
+				// Return 'true' iff all the requested keys are pressed.
+				return keys.All(key => keyboardState[(int) key] >= 128);
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Useful if you have a qualifier and a value you want to test, for example
+		/// A and B  pressed together means do something, but it is the next key tells
+		/// you what.
+		/// </summary>
+		public static bool AreKeysPressed(VirtualKey[] qualifiers, VirtualKey key)
+		{
+			if (qualifiers == null) { return false; }
+
+			// This only returns 'true' when the qualifiers are pressed and key is 
+			// not one of the qualifiers.
+			return AreKeysPressed(qualifiers) && IsKeyPressed(key) && !qualifiers.Contains(key);
+		}
+
+		/// <summary>
+		/// Useful when handling mouse clicks.  Note that the VirtualKey enumeration
+		/// does not define values for every int less than 256.
+		/// </summary>
+		public static VirtualKey[] GetPressedKeys()
+		{
+			var list = new List<VirtualKey>();
+
+			var keyboardState = new byte[256];
+
+			// GetKeyboardState returns 'true' iff successful.
+			if (GetKeyboardState(keyboardState))
+			{
+				for (var i = 0; i < keyboardState.Length; i++)
+				{
+					if (keyboardState[i] > 127) { list.Add((VirtualKey)i); }
+				}
+			}
+
+			return list.ToArray();
+		}
+	}
+}
